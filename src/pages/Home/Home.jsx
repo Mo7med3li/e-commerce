37ed1f08d2d11586abf8ebd77405c993d6c64edf
nohamed3li <mmkandeelz74@gmail.com>
2024-12-01 04:08:41 +0200

    static card design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
+import Card from "../../components/Card/Card";
 import Loading from "../../components/Loading/Loading";
 
 export default function Home() {
   return (
     <>
-      <Loading />
+      <div className="grid sm:grid-cols-2 sm:gap-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+        <Card />
+      </div>
     </>
   );
 }
