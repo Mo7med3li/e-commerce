56654937020f8dc6ea057d7d180049097378f867
nohamed3li <mmkandeelz74@gmail.com>
2024-12-01 04:55:18 +0200

    display products

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,37 +1,36 @@
-import google from "../../assets/images/get-google-play.png";
-
-export default function Card() {
+export default function Card({ productInfo }) {
+  const { category, description, imageCover, title, price, ratingsAverage } =
+    productInfo;
   return (
     <>
       <div className="card group/card rounded-lg shadow-lg overflow-hidden ">
         <div className=" relative ">
-          <img src={google} alt="" className="object-cover" />
+          <img src={imageCover} alt="" className="object-cover" />
           <div className="card-layer absolute inset-0 w-full h-full bg-slate-400 flex items-center justify-center gap-3 bg-opacity-40 opacity-0 group-hover/card:opacity-100 transition-opacity duration-300">
             <div className="icon cursor-pointer bg-primary-600 text-white w-7 h-7 flex justify-center items-center rounded-full">
-              <i class="fa-solid fa-heart "></i>
+              <i className="fa-solid fa-heart "></i>
             </div>
             <div className="icon cursor-pointer bg-primary-600 text-white w-7 h-7 flex justify-center items-center rounded-full">
-              <i class="fa-solid fa-cart-shopping "></i>
+              <i className="fa-solid fa-cart-shopping "></i>
             </div>
             <div className="icon cursor-pointer bg-primary-600 text-white w-7 h-7 flex justify-center items-center rounded-full">
-              <i class="fa-solid fa-eye "></i>
+              <i className="fa-solid fa-eye "></i>
             </div>
           </div>
         </div>
         <div className="card-body space-y-3 p-4">
           <header className="space-y-1">
-            <h2 className=" text-primary-600 font-semibold">Category </h2>
-            <h3 className="text-xl text-slate-800 font-semibold">Tittle</h3>
+            <h2 className=" text-primary-600 font-semibold">{category.name}</h2>
+            <h3 className="text-xl text-slate-800 font-semibold line-clamp-1">
+              {title}
+            </h3>
           </header>
-          <p className="line-clamp-2 text-sm text-gray-500 ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-            ipsa.
-          </p>
+          <p className="line-clamp-2 text-sm text-gray-500 ">{description}</p>
           <div className="flex justify-between">
-            <span>price</span>
+            <span>{price}E</span>
             <div>
-              <i class="fa-solid fa-star text-yellow-500 mr-1"></i>
-              <span>4.8</span>
+              <i className="fa-solid fa-star text-yellow-500 mr-1"></i>
+              <span>{ratingsAverage}</span>
             </div>
           </div>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,34 @@
+import axios from "axios";
 import Card from "../../components/Card/Card";
 import Loading from "../../components/Loading/Loading";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [products, setProducts] = useState(null);
+  async function getProducts() {
+    const options = {
+      url: "https://ecommerce.routemisr.com/api/v1/products",
+      method: "GET",
+    };
+    let { data } = await axios.request(options);
+    console.log(data.data);
+    setProducts(data.data);
+  }
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <>
-      <div className="grid sm:grid-cols-2 sm:gap-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-        <Card />
-      </div>
+      {products === null ? (
+        <Loading />
+      ) : (
+        <div className="grid sm:grid-cols-2 sm:gap-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+          {products.map((products) => (
+            <Card productInfo={products} key={products.id} />
+          ))}
+        </div>
+      )}
     </>
   );
 }
